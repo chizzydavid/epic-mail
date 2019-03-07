57cabe7d07e30ab38267ff601f4acd086353cb66
Chizzy David <davidchizindu@gmail.com>
2019-03-06 20:01:51 -0800

    Added API endpoint to get all Users.

--- server/dummy/controllers/User.js
+++ server/dummy/controllers/User.js
@@ -11,23 +11,29 @@ const User = {
     return res.status(201).json({ status: 201, data: { ...newUser } });
   },
 
+  loginUser(req, res) {
+    const { email, password } = req.body;
+
+    if (!email || !password)
+      return res.status(400).json({ status: 400, error: 'All fields are required.' });
+
+    const userLogin = UserModel.login({ email, password });
+    if (userLogin.message === 'User not found')
+      return res.status(404).json({ status: 404, error: 'User not found.' });
+
+    if (userLogin.message === 'Invalid password')
+      return res.status(400).json({ status: 400, error: 'Invalid password.' });
+
+    return res.status(200).json({ status: 200, data: { ...userLogin } });
+  },
+
+  getAllUsers(req, res) {
+    const users = UserModel.findAll();
+    return res.status(200).json({ status: 200, data: [...users] });
+  },
 
 
-	loginUser(req, res) {
-	  const { email, password } = req.body;
 
-	  if (!email || !password)
-	    return res.status(400).json({ status: 400, error: 'All fields are required.' });
-
-	  const userLogin = UserModel.login({ email, password });
-	  if (userLogin.message === 'User not found')
-	    return res.status(404).json({ status: 404, error: 'User not found.' });
-
-	  if (userLogin.message === 'Invalid password')
-	    return res.status(400).json({ status: 400, error: 'Invalid password.' });
-
-	  return res.status(200).json({ status: 200, data: { ...userLogin } });
-    },
 };
 
 export default User;
