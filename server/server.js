abd09672c1bbab53b8ec73da58f37a1911754857
Chizzy David <davidchizindu@gmail.com>
2019-03-09 03:49:11 -0800

    updated server.js file

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(routes);
 
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`App running at port ${PORT}`);
 });
