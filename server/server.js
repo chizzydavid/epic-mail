f9eadfd849e8f204eea21a7ce7ab49fc19137b59
Chizzy David <davidchizindu@gmail.com>
2019-03-09 03:34:56 -0800

    added dotenv configuration to server.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
 import routes from './routes/index';
 
+dotenv.config();
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
