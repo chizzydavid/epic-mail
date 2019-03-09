ee3422340ac8a592c2f3113766bfcb429650b665
Chizzy David <davidchizindu@gmail.com>
2019-03-09 03:59:03 -0800

    update package json file

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(routes);
+app.set('port', process.env.PORT || 5000);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
