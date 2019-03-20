14c84126296eae2c203c89aa115827cf70389abd
Chizzy David <davidchizindu@gmail.com>
2019-03-19 21:51:00 -0700

    Created endpoint to get all users from the database

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,14 @@
 import { Router } from 'express';
 import User from '../controllers/User';
 import Validate from '../middlewares/validation/validation';
+import Auth from '../middlewares/Auth';
 
 const router = Router();
 
 // setup api routes/endpoints
 router.post('/api/v2/auth/signup', Validate.signUp, User.createUser);
 router.post('/api/v2/auth/login', Validate.login, User.loginUser);
+router.get('/api/v2/users', Auth.verifyToken, User.getAllUsers);
 
 
 export default router;
