1524ef1b6bfbf25c5c5db1810f7c3b87d8d5b71b
Chizzy David <davidchizindu@gmail.com>
2019-03-19 20:10:27 -0700

    Edited files for setting up tables for testing on travis-ci

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,10 +6,6 @@ const router = Router();
 
 // setup api routes/endpoints
 router.post('/api/v1/auth/signup', Validate.signUp, User.createUser);
-router.post('/api/v1/auth/login', Validate.login, User.loginUser);
-router.get('/api/v1/users', User.getAllUsers);
-router.get('/api/v1/users/:id', User.getSingleUser);
-router.delete('/api/v1/users/:id', User.deleteUser);
 
 
 export default router;
